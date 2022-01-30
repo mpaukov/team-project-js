949f5bd8f3f3659d0757a7365e4cc1bb4a78c261
MaksymChuraiev <87321814+MaksymChuraiev@users.noreply.github.com>
2022-01-30 15:27:52 +0000

    Deploying to gh-pages from @ MaksymChuraiev/team-project-js@f201569017986dba6f3e4cd492420fb222d4b1cb 🚀

--- src.f8c55115.js
+++ src.f8c55115.js
@@ -105,4 +105,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/components/header"),require("./js/components/switcher"),require("./js/components/scroll"),require("./js/components/fetchApi"),require("./js/components/gallery"),require("./js/components/pagination"),require("./js/components/modal"),require("./js/components/teaser");
 },{"./sass/main.scss":"clu1","./js/components/header":"NNYM","./js/components/switcher":"bw8R","./js/components/scroll":"r7Tq","./js/components/fetchApi":"CwY4","./js/components/gallery":"QUdn","./js/components/pagination":"H1oe","./js/components/modal":"fSkc","./js/components/teaser":"EIKK"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-js/src.eaf1e016.js.map
+//# sourceMappingURL=/team-project-js/src.f8c55115.js.map
