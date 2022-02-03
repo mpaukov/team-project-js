b7dfd82265a0676ea1daf827a2ad9d74eb159e45
MaksymChuraiev <87321814+MaksymChuraiev@users.noreply.github.com>
2022-02-03 21:37:32 +0000

    Deploying to gh-pages from @ MaksymChuraiev/team-project-js@468134eb12c0fe66daff6187d6a41ed36e7f6805 🚀

--- src.fa1d7baa.js
+++ src.fa1d7baa.js
@@ -131,4 +131,4 @@ const e={footerLink:document.querySelector(".footer__link"),closeModalBtn:docume
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/components/header"),require("./js/components/switcher"),require("./js/components/scroll"),require("./js/components/fetchApi"),require("./js/components/gallery"),require("./js/components/pagination"),require("./js/components/modal"),require("./js/components/teaser"),require("./js/components/team-modal"),require("./js/components/library"),require("./js/components/home"),require("./js/components/filterBtn");
 },{"./sass/main.scss":"clu1","./js/components/header":"NNYM","./js/components/switcher":"bw8R","./js/components/scroll":"r7Tq","./js/components/fetchApi":"CwY4","./js/components/gallery":"QUdn","./js/components/pagination":"H1oe","./js/components/modal":"fSkc","./js/components/teaser":"EIKK","./js/components/team-modal":"Cw5H","./js/components/library":"wZ4W","./js/components/home":"wBjs","./js/components/filterBtn":"fmEK"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-js/src.3e2fa0ff.js.map
+//# sourceMappingURL=/team-project-js/src.fa1d7baa.js.map
